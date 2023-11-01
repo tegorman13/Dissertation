084d9ccb3d494c04b24c588ceea210f3e0687852
Thomas <tegorman13@gmail.com>
2023-10-31 19:13:49 -0500

    clean up repo

--- md_extract/remove_links.py
+++ md_extract/remove_links.py
@@ -7,8 +7,14 @@ def remove_links(filename):
 
     new_lines = []
     for line in lines:
-        # Using regex to remove the localhost links
+        # Using regex to remove localhost links
         new_line = re.sub(r'\[([^\]]+)\]\(http://localhost:[0-9]+/[^)]+\)', r'\1', line)
+        # Using regex to remove links containing "#ref"
+        new_line = re.sub(r'\[([^\]]+)\]\([^)]*#ref[^)]*\)', r'\1', new_line)
+        new_line = re.sub(r'\[([^\]]+)\]\([^)]*#tbl[^)]*\)', r'\1', new_line)
+        new_line = re.sub(r'\[([^\]]+)\]\([^)]*#fig[^)]*\)', r'\1', new_line)
+        # Using regex to remove image links with pattern [![](image_source)](link)
+        new_line = re.sub(r'\[\!\[\]\([^\)]+\)\]\([^\)]+\)', '', new_line)
         new_lines.append(new_line)
 
     with open(filename, 'w') as file:
@@ -20,4 +26,4 @@ if __name__ == "__main__":
     else:
         filename = sys.argv[1]
         remove_links(filename)
-        print(f"Removed localhost links from {filename}.")
+        print(f"Removed localhost links, #ref links, and image links from {filename}.")
